932055c2ac82a8b021a7e127d433639052e078c6
LukeNzk <luke.nizik@gmail.com>
2020-11-08 20:43:03 +0100

    stage 5 - caching position

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -7,25 +7,27 @@ public class Pawn : MonoBehaviour
 {
     private Vector3 _dir = Vector3.zero;
     private Vector3 _center;
+    private Vector3 _position;
     private Transform _transform;
     private float _speed = 0.2f;
     
-
     void Start()
     {
         _center = Random.insideUnitSphere;
         _transform = transform;
+        _position = _transform.position;
     }
     
     public void Move()
     {
-        Vector3 toCenter = _center - _transform.position;
+        Vector3 toCenter = _center - _position;
         toCenter *= Mathf.Exp(toCenter.magnitude) / 100.0f;
         
         Vector3 randomDir = Random.onUnitSphere + _dir * 20.0f + toCenter;
         randomDir.Normalize();
         _dir = randomDir;
-        
-        _transform.position += randomDir * _speed * Time.deltaTime;
+
+        _position += randomDir * _speed * Time.deltaTime;
+        _transform.position = _position;
     }
 }
